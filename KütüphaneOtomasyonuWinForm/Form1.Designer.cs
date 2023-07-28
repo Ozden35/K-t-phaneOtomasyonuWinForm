02e2d6671b772ac2afb65953424e324dcc3887fb
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-07-28 23:44:44 +0300

    Proje dosyası ekle.

---
+++ KütüphaneOtomasyonuWinForm/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace KütüphaneOtomasyonuWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.adGiristxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sifreGiristxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.personelGirisbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adGiristxt
+            // 
+            this.adGiristxt.Location = new System.Drawing.Point(168, 225);
+            this.adGiristxt.Name = "adGiristxt";
+            this.adGiristxt.Size = new System.Drawing.Size(257, 22);
+            this.adGiristxt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad :";
+            // 
+            // sifreGiristxt
+            // 
+            this.sifreGiristxt.Location = new System.Drawing.Point(168, 268);
+            this.sifreGiristxt.Name = "sifreGiristxt";
+            this.sifreGiristxt.Size = new System.Drawing.Size(257, 22);
+            this.sifreGiristxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre :";
+            // 
+            // personelGirisbtn
+            // 
+            this.personelGirisbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.personelGirisbtn.Location = new System.Drawing.Point(168, 322);
+            this.personelGirisbtn.Name = "personelGirisbtn";
+            this.personelGirisbtn.Size = new System.Drawing.Size(257, 36);
+            this.personelGirisbtn.TabIndex = 4;
+            this.personelGirisbtn.Text = "Giriş";
+            this.personelGirisbtn.UseVisualStyleBackColor = false;
+            this.personelGirisbtn.Click += new System.EventHandler(this.personelGirisbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(237, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 90);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(539, 470);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.personelGirisbtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sifreGiristxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.adGiristxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox adGiristxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sifreGiristxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button personelGirisbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ KütüphaneOtomasyonuWinForm/İslemPaneli.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace KütüphaneOtomasyonuWinForm
+{
+    partial class İslemPaneli
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(İslemPaneli));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.kullaniciBilgiPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(268, 735);
+            this.panel1.TabIndex = 0;
+            // 
+            // kullaniciBilgiPanel
+            // 
+            this.kullaniciBilgiPanel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.kullaniciBilgiPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kullaniciBilgiPanel.Location = new System.Drawing.Point(268, 0);
+            this.kullaniciBilgiPanel.Name = "kullaniciBilgiPanel";
+            this.kullaniciBilgiPanel.Size = new System.Drawing.Size(1074, 44);
+            this.kullaniciBilgiPanel.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kullanıcılar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 72);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // İslemPaneli
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1342, 735);
+            this.Controls.Add(this.kullaniciBilgiPanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "İslemPaneli";
+            this.Text = "İslemPaneli";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel kullaniciBilgiPanel;
+        private System.Windows.Forms.Button button2;
+    }
+}
